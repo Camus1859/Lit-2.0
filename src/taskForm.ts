e2f65094f0b306bfe378e0c41e4525702efc1eb9
Anderson Laventure <Laventurepsu1@gmail.com>
2026-06-17 16:51:29 -0400

    feat: create TaskForm component with basic structure

---
+++ src/taskForm.ts
@@ -0,0 +1,11 @@
+import { LitElement, html, css } from "lit";
+import { property, customElement } from "lit/decorators.js";
+
+@customElement("task-form")
+export class TaskForm extends LitElement {
+  static styles = css``;
+
+  render(){
+    return html``
+  }
+}
