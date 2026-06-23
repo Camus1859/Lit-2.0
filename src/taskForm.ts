0e28c79dd00420d07fdddacff8952aea0e0758b6
Anderson Laventure <Laventurepsu1@gmail.com>
2026-06-23 17:04:50 -0400

    feat: enhance TaskForm component with state management for title, description, and priority inputs

--- src/taskForm.ts
+++ src/taskForm.ts
@@ -1,38 +1,111 @@
 import { LitElement, html, css } from "lit";
-import { property, customElement, query } from "lit/decorators.js";
+import { property, customElement, query, state } from "lit/decorators.js";
 
 @customElement("task-form")
 export class TaskForm extends LitElement {
   static styles = css``;
-  @query(".title-input", true) _titleInput!: HTMLInputElement;
-  @query(".description-text-area", true) _descriptionTextArea!: HTMLTextAreaElement;
-  @query(".priority-input", true) _priorityInput!: HTMLInputElement;
+
+  @state()
+  title: string = "";
+
+  @state()
+  description: string = "";
+
+  priority: string = "";
 
   render() {
     return html`<div>
+      <div>
+        <p>Title:</p>
+        <input
+          .value=${this.title}
+          @input=${(e: Event) => {
+            if (e.target) {
+              const target = e.target as HTMLInputElement;
+              this.title = target.value;
+            }
+          }}
+          class="title-input"
+        />
+      </div>
 
+      <div>
+        <p>Description:</p>
+        <label for="description"></label>
+        <textarea
+          .value=${this.description}
+          @input=${(e: Event) => {
+            if (e.target) {
+              const target = e.target as HTMLTextAreaElement;
+              this.description = target.value;
+            }
+          }}
+          class="description-text-area"
+          id="description"
+          name="description"
+          rows="5"
+          cols="40"
+          placeholder="Enter your description here..."
+        ></textarea>
 
-    <p>Title:</p>
-    <input class="title-input"></input>
-
-    <label for="description">Description:</label>
-    <textarea class="description-text-area" id="description" name="description" rows="5" cols="40" placeholder="Enter your description here..."></textarea>
-
-    <p>Priority:</p>
-    <input class="priority-input"></input>
-
-    <button @click=${this._addTask}>Add Task</button>
-    
+        <div>
+          <label for="priority-select">Priority:</label>
+          <select
+            name="priorties"
+            id="priority-select"
+            .value=${this.priority}
+            @change=${(e: Event) => {
+              const target = e.target as HTMLSelectElement;
+              this.priority = target.value;
+            }}
+          >
+            <option value="">--Please choose an option--</option>
+            <option value="high">High</option>
+            <option value="medium">Medium</option>
+            <option value="low">Low</option>
+          </select>
+        </div>
+        <button @click=${this._addTask}>Add Task</button>
+      </div>
     </div>`;
   }
 
   private _addTask() {
-    const title = this._titleInput.value.trim();
-    const description = this._descriptionTextArea.value.trim();
-    const priority = this._priorityInput.value.trim();
+    if (!this._validation()) {
+      return;
+    }
 
-    console.log(title);
-    console.log(description);
-    console.log(priority);
+    const task = {
+      id: Math.random().toString(),
+      title: this.title,
+      description: this.description,
+      priority: this.priority,
+      status: "todo",
+    };
+
+    const options = {
+      detail: { task },
+      bubbles: true,
+      composed: true,
+    };
+
+    this.dispatchEvent(new CustomEvent("add-task", options));
+
+    this.title = "";
+    this.description = "";
+    this.priority = "";
+
+    console.log(task);
+  }
+
+  private _validation() {
+    if (
+      this.title.trim() === "" ||
+      this.description.trim() === "" ||
+      this.priority.trim() === ""
+    ) {
+      return false;
+    }
+    return true;
   }
 }
